2cdbe3feec82488ed25af29ef72a765caffd192a
Tlaster <tlaster@outlook.com>
2021-06-02 14:21:37 +0800

    spotless cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,12 +5,9 @@ pluginManagement {
         mavenCentral()
         maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
     }
-    
 }
 rootProject.name = "ComChat"
-
 
 include(":android")
 include(":desktop")
 include(":common")
-
